bf8d29429a7c17aa8a444fa0b1dd6b7c514d7eaa
CCTsai <px248617935@gmail.com>
2021-09-16 15:26:21 +0800

    完成記分板功能: 1.下一局 2.強調已完成局數 3.勝局數累計

--- public/app.js
+++ public/app.js
@@ -7,15 +7,11 @@ let scoreMax = parseInt(playTo.value);
 let endGame = false;
 let gCurrent = 1;   // current games
 let deuce = false;
+let gameCalled = false;
 
 const rematchBtn = document.getElementById('rematchBtn');
 const saveBtn = document.getElementById('saveBtn');
 const nextBtn = document.getElementById('nextBtn');
-
-// const t1Score = document.getElementById('t1Score');
-// const t2Score = document.getElementById('t2Score');
-// const t1Table = document.getElementById('t1Table');
-// const t2Table = document.getElementById('t2Table');
 
 const scoreTable = document.getElementById('scoreTable');
 const tableHead = document.getElementById('tableHead');
@@ -25,36 +21,11 @@ const winBar = document.getElementById('winBar');
 const cleanBtn = document.getElementById('cleanBtn');
 const allScore = document.querySelectorAll('.cl-score');    // for cleaning all score
 
-// const t1Plus = document.getElementById('t1Plus');
-// const t1Minus = document.getElementById('t1Minus');
-// const t2Plus = document.getElementById('t2Plus');
-// const t2Minus = document.getElementById('t2Minus');
-
-// const t1 = {
-//     teamBtn: document.getElementById('t1Button'),
-
-//     nameInput: document.getElementById('t1NameInput'),
-//     editor: document.getElementById('t1Edit'),
-//     adder: document.getElementById('t1Add'),
-//     remover: document.getElementById('t1Remove'),
-
-//     mates: document.getElementById('t1Mates')
-// }
-
-// const t2 = {
-//     teamBtn: document.getElementById('t2Button'),
-
-//     nameInput: document.getElementById('t2NameInput'),
-//     editor: document.getElementById('t2Edit'),
-//     adder: document.getElementById('t2Add'),
-//     remover: document.getElementById('t2Remove'),
-
-//     mates: document.getElementById('t2Mates')
-// }
-
 class teams {
     constructor(t) {
         this.score = 0;
+        this.goal = 0;
+        this.winner = 0;
 
         this.table = document.getElementById(t+'Table');    // score table
         
@@ -77,58 +48,69 @@ t1 = new teams('t1');   // team 1
 t2 = new teams('t2');   // team 2
 
 /*-------------------------------
-     Controllers about:
-        1. game setting
-        2. score table
-        3. clean all score
+     Game setting:
+        1. games
+        2. max score
+        3. rematch
+        4. clean all score
+        5. next game
 ---------------------------------*/
 gameSet.addEventListener('change', function () {
     gameMax = parseInt(this.value);
-    gCurrent = 1;
-    setTable(gameMax, tableHead);
-    setTable(gameMax, t1.table);
-    setTable(gameMax, t2.table);
+    tableIni(tableHead);
+    tableIni(t1.table);
+    tableIni(t2.table);
+    setButton();
     cleanAllScore();
-    setButton(gameMax);
 })
 
 playTo.addEventListener('change', () => {
     scoreMax = parseInt(playTo.value);
-    setButton(gameMax);
+    setButton();
     cleanAllScore();
+})
+
+rematchBtn.addEventListener('click', () => {
+    rematch();
+    setButton();
 })
 
 cleanBtn.addEventListener('click', (e) => {
     e.preventDefault();
     gameMax = parseInt(gameSet.value);
-    gCurrent = 1;
-    setButton(gameMax);
+    setButton();
     cleanAllScore();
 })
 
+nextBtn.addEventListener('click', () => {
+    nextGame();
+})
+
+
+
 // show or hide table elements
-const setTable = (games, tb) => {
-    if (games === 1) {
+const tableIni = (tb) => {
+    if ( gameMax === 1) {
         for(let i of tb.children) {
             i.classList.add('collapse');
         }
-    } else if(games === tb.childElementCount) {
+    } else if( gameMax === tb.childElementCount) {
         for(let j of tb.children) {
             j.classList.remove('collapse');
         }
     } else {
-        for(let k=0; k<games; k++) {
+        for(let k=0; k<gameMax; k++) {
             tb.children[k].classList.remove('collapse');
         }
-        for(let m=games; m<tb.childElementCount; m++) {
+        for(let m=gameMax; m<tb.childElementCount; m++) {
             tb.children[m].classList.add('collapse');
         }
     }
 }
 
 // show or hide save botton & next game button
-const setButton = (games) => {
-    if( games === gCurrent) {
+const setButton = () => {
+    if( gameMax === gCurrent) {
         nextBtn.classList.add('collapse');
         saveBtn.classList.remove('collapse');
     } else {
@@ -137,25 +119,106 @@ const setButton = (games) => {
     }
 }
 
+// insert score to table
+const tableInsert = () => {
+    t1.table.children[gCurrent-1].innerText = t1.score;
+    t2.table.children[gCurrent-1].innerText = t2.score;
+}
+
+// highlight table head
+const tableHighlight = () => {
+    tableHead.children[gCurrent-1].classList.add('table-dark');
+}
+
+const tableDark = () => {
+    for(let i=1; i<gameMax; i++){
+        tableHead.children[i].classList.remove('table-dark');
+    }
+}
+
+// next game
+const nextGame = async () => {
+    //await tableInsert();
+    await gCurrent++;
+    tableHighlight();
+    setButton();
+
+    for(let i of [t1, t2]) {
+        i.score = 0;
+        i.scoreValue.value = 0;
+        i.minusBtn.disabled = true;
+        i.winner = 0;
+    }
+    tableInsert();
+    ending(0)
+    deuceCheck();
+    rematchBtn.disabled = true;
+}
+
+// end game setting
+const ending = (i) => {
+    t1.plusBtn.disabled = i;
+    t2.plusBtn.disabled = i;
+    endGame = !!i;
+    saveBtn.disabled = !i;
+    nextBtn.disabled = !i;
+    if( endGame ) {
+        winBar.classList.remove('collapse');
+    } else {
+        winBar.classList.add('collapse');
+    }
+    if(Math.max(t1.goal, t2.goal) > gameMax/2 && gCurrent > 1 && t1.goal+t2.goal != gameMax) {
+        callGame(i);
+    }
+}
+
+// call game
+const callGame = (i) => {
+    if( i === 0 && gameCalled) {
+        //console.log('call game false');
+        gameCalled = false;
+        gCurrent = t1.goal + t2.goal;
+    } else {
+        //console.log('call game true');
+        gameCalled = true;
+        gCurrent = gameMax;
+    }
+    setButton();
+}
+
+// clean current score
+const rematch = () => {
+    ending(0)
+    for(let j of [t1, t2]) {
+        j.goal -= j.winner;
+        j.winner = 0;
+    }
+    for(let i of [t1, t2]) {
+        i.score = 0;
+        i.scoreValue.value = 0;
+        i.minusBtn.disabled = true;
+    }
+    tableInsert();
+    deuceCheck();
+    rematchBtn.disabled = true;
+}
+
 // clean all score
 const cleanAllScore = () => {
     for(let i of allScore) {
         i.value = 0;
         i.innerText = '0';
     }
+    t1.winner = 0;
+    t1.goal = 0;
+    t2.winner = 0;
+    t2.goal = 0;
+    gCurrent = 1;
+    tableDark();
     setButton(gameMax);
     rematch();
 }
 
-// highlight table head
-const highlightTable = (gNext) => {
-    if(gNext === 0) {
-        tableHead.children[gCurrent-1].classList.remove('table-dark');
-        tableHead.children[0].classList.add('table-dark');
-    }
-    tableHead.children[gCurrent].classList.remove('table-dark');
-    tableHead.children[gNext].classList.add('table-dark');
-}
 
 
 /*---------------------
@@ -163,7 +226,6 @@ const highlightTable = (gNext) => {
         1. plus
         2. minus
         3. deuce
-        4. rematch
 ----------------------*/
 t1.plusBtn.addEventListener('click', () => {
     plusOne(t1, t2);
@@ -181,50 +243,64 @@ t2.minusBtn.addEventListener('click', () => {
     minusOne(t2, t1);
 })
 
-rematchBtn.addEventListener('click', () => {
-    rematch();
-    setButton(gameMax);
-})
 
-
-function plusOne(team, another) {
+const plusOne = (team, another) => {
+    rematchBtn.disabled = false;
     if( !endGame ){
         team.score++;
         team.minusBtn.disabled = false;
         team.scoreValue.value = team.score;
+        tableInsert();
         if( !deuce ) {
             deuceCheck();
-            if(team.score === scoreMax) {
+            if( team.score === scoreMax) {
+                team.winner = 1;
+                team.goal++;
                 ending(1);
             }
-        } else if(deuce && team.score > another.score+1) {
+        } else if( deuce && team.score > another.score+1) {
             another.minusBtn.disabled = true;
+
+            team.winner = 1;
+            team.goal++;
             ending(1);
+
             deuceCheck();
         }
     }
 }
 
-function minusOne(team, another) {
+const minusOne = (team, another) => {
     if( endGame ) {
         if( team.score === scoreMax) {
             team.score--;
             team.scoreValue.value = team.score;
+            
             ending(0);
+            team.winner = 0;
+            team.goal--;
         } else {
             team.score--;
             team.scoreValue.value = team.score;
             deuceCheck();
             another.minusBtn.disabled = false;
+            
             ending(0);
+            team.winner = 0;
+            team.goal--;
         }
+        tableInsert();
     } else {
-        if(deuce) {
+        if( deuce ) {
             team.score--;
             team.scoreValue.value = team.score;
+            tableInsert();
             deuceCheck();
-            if(another.score === scoreMax | team.score >= scoreMax && !deuce) {
+            if( another.score === scoreMax | team.score >= scoreMax-1 && !deuce) {
                 team.minusBtn.disabled = true;
+                
+                another.winner = 1;
+                another.goal++;
                 ending(1);
             }
         } else {
@@ -232,28 +308,19 @@ function minusOne(team, another) {
             team.scoreValue.value = team.score;
         }
     }
+    //tableInsert();
 
     if( team.score === 0) {
         team.minusBtn.disabled = true;
     }
-}
 
-// end game setting
-function ending(i) {
-    t1.plusBtn.disabled = i;
-    t2.plusBtn.disabled = i;
-    endGame = !!i;
-    saveBtn.disabled = !i;
-    nextBtn.disabled = !i;
-    if(endGame) {
-        winBar.classList.remove('collapse');
-    } else {
-        winBar.classList.add('collapse');
+    if( t1.score === 0 && t2.score === 0) {
+        rematchBtn.disabled = true;
     }
 }
 
-function deuceCheck() {
-    if(t1.score >= scoreMax-1 && t2.score >= scoreMax-1 && Math.abs(t1.score-t2.score)<2 ) {
+const deuceCheck = () => {
+    if( t1.score >= scoreMax-1 && t2.score >= scoreMax-1 && Math.abs(t1.score-t2.score)<2 ) {
         deuceBar.classList.remove('collapse');
         deuce = true;
     } else {
@@ -262,16 +329,7 @@ function deuceCheck() {
     }
 }
 
-// clean current score
-function rematch() {
-    for(let i of [t1, t2]) {
-        i.score = 0;
-        i.scoreValue.value = 0;
-        i.minusBtn.disabled = true;
-        ending(0)
-        deuceCheck();
-    }
-}
+
 
 /*-----------------------------
     Team controllers:
