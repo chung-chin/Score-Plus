809656aa41e37692184f1ed3d75a07863c0011c1
cctsai <px248617935@gmail.com>
2021-11-09 18:23:51 +0800

    更改路由

--- index.js
+++ index.js
@@ -4,22 +4,34 @@ const path = require('path');
 const mongoose = require('mongoose');
 const { render } = require('ejs');
 
+mongoose.connect('mongodb://localhost:27017/games', {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => {
+        console.log("Mongo connection open !!")
+    })
+    .catch(err => {
+        console.log(err)
+    })
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true}));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
-app.get('/', (req, res) => {
-    res.render('score_plus', { pageName: "Score Plus" })
-})
 
 // app.get('/score_now', (req, res) => {
 //     res.render('score_now', { pageName: "Score Now" })
 // })
 
-app.get('/ping_pong', (req, res) => {
-    res.render('ping_pong', { pageName: "Ping Pong" })
+app.get('/scoreplus', (req, res) => {
+    const { q } = req.query;
+    if (q) {
+        // console.log(q)
+        const pName = q.replace("_", " ")
+        // console.log(pName)
+        res.render(q, { pageName: pName})
+    } else {
+        res.render('score_plus', { pageName: "Score Plus"})
+    }
 })
 
 app.listen(3000, () => {
